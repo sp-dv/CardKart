51212a61c542903355fff70ad630b6f918eb7813
sp-dv <sebastiandeverdier@gmail.com>
2022-01-06 17:18:02 -0500

    Back to dev

--- Shared/src/Util/Constants.cs
+++ Shared/src/Util/Constants.cs
@@ -9,9 +9,9 @@ namespace CardKartShared.Util
 {
     public static class Constants
     {
-        // Last release: 0.0.1
-        public const string Version = "0.0.2";
-        public static bool IsDevVersion = false;
+        // Last release: 0.0.2
+        public const string Version = "0.0.0";
+        public static bool IsDevVersion = true;
 
         private static RSAParameters DebugPublicKey => 
             JsonConvert.DeserializeObject<RSAParameters>("{\"D\":null,\"DP\":null,\"DQ\":null,\"Exponent\":\"AQAB\",\"InverseQ\":null,\"Modulus\":\"xt0Ce6WPiB2C9hFlRANg7cV96tYb/6QwC3zQaleQZUuchwnhIlMFWgKVECfUfGTXu/UrPa0JeG6kyJQycXnYJYTc6PO08I5BxuFfRR1rNcWmM3pWidJhTuFPgv0G+1J+elsGfS4K641OI+ZNgw/V1Hq1bZOl3XXKWKaWLOFvsKE=\",\"P\":null,\"Q\":null}");
